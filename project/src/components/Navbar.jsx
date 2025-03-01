7823c3b0a34c7866598ae5c4ca5935379e44ce22
your name <your-email@example.com>
2025-03-01 16:52:55 +0530

    Feat: Added Navbar component with links and avatar

--- project/src/components/Navbar.jsx
+++ project/src/components/Navbar.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
+import logo from "../images/logo.png"
+import { Link } from 'react-router-dom'
+import Avatar from 'react-avatar';
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className="navbar flex items-center justify-between px-[100px] h-[80px] bg-[#141414]">
+       <div className="logo">
+          <img className='w-[150px]  cursor-pointer' src={logo} alt=""/>
+        </div>
+        <div className="links flex items-center gap-2">
+          <Link>Home</Link>
+          <Link>About</Link>
+          <Link>Contact</Link>
+          <Link>Services</Link>
+          <Avatar name="Wim Mostmans" size="150" />
+          {/* <button onClick={logout} className='btnBlue !bg-red-500 min-w-[120px] ml-2 hover:!bg-red-600'>Logout</button> */}
+          {/* <Avatar onClick={() => { toggleClass(".dropDownNavbar", "hidden") }} name={data ? data.name : ""} size="40" round="50%" className=' cursor-pointer ml-2' /> */}
+        </div>
+    </div>
   )
 }
 
