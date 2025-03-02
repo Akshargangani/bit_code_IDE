da58b226ed5564b961bf6bba51766f30a5ed529c
your name <your-email@example.com>
2025-03-02 10:28:56 +0530

    Added ListCard component with styling and images

--- project/src/components/ListCard.jsx
+++ project/src/components/ListCard.jsx
@@ -1,13 +1,24 @@
 import React from 'react'
 import img from "../images/code.png"
+import deleteImg from "../images/delete.png"
 
 const ListCard = () => {
   return (
     <div className="listCard mb-2 w-[full] flex items-center justify-between p-[10px] bg-[#141414] cursor-pointer rounded-lg hover:bg-[#202020]">
         <div className='flex items-center gap-2'>
-            <img className='' src={img} alt="" />
+            <img className='w-[80px]' src={img} alt="" />
+        </div>
+        <div>
+          <h3 className='text-[20px]'>My first project</h3>
+          <p className='text-[gray] text-[14px]'>create in 3 mon 2025</p>
+        </div>
+        <div>
+        <img  className ="w-[30px] cursor-pointer mr-4" src= {deleteImg} alt="" />
         </div>
     </div>
+   
+
+    
   )
 }
 
