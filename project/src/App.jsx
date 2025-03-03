ec52c9521b35fed922f70283f6da0bdeb42f7658
your name <your-email@example.com>
2025-03-03 11:30:25 +0530

    Set up React Router with Home, SignUp, Login, Editior, and NoPage routes

--- project/src/App.jsx
+++ project/src/App.jsx
@@ -5,6 +5,8 @@ import Home from './pages/Home.jsx'
 import NoPage from './pages/NoPage.jsx';
 import SignUp from './pages/SignUp.jsx';
 import Login from './pages/Login.jsx';
+import Editior from './pages/Editior.jsx';
+
 const App = () => {
   return (
     <BrowserRouter>
@@ -12,6 +14,7 @@ const App = () => {
       <Route path='/' element={<Home /> }/>
       <Route path='/signUp' element={<SignUp />} />  
        <Route path='/Login' element={<Login />} />
+       <Route path='/editior/:projectID' element={<Editior />} />
        {/* <Route path='/editior/:projectID' element={isLoggedIn ? <Editior /> : <Navigate to="/Login"/>} />  */}
       <Route path="*" element={<NoPage />} />
     </Routes>
