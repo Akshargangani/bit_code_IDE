492c6fa68928e95fe168fc027275ead29c62a545
your name <your-email@example.com>
2025-03-03 19:29:29 +0530

    Improved Home component: Added project creation modal, refined layout, and enhanced UI interactions

--- project/src/pages/Home.jsx
+++ project/src/pages/Home.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { useState } from "react";
 import Navbar from "../components/Navbar.jsx";
 import ListCard from "../components/ListCard.jsx";
@@ -6,6 +5,7 @@ import GridCard from "../components/GridCard.jsx";
 
 const Home = () => {
   const [isGridLayout, setIsGridLayout] = useState(false);
+  const [isCreateModelShow, setIsCreateModelShow] = useState(false);
 
   return (
     <>
@@ -21,10 +21,7 @@ const Home = () => {
           
           />
         </div>
-        <button
-          className="btnBlue rounded-[5px] mb-4 text-[20px] !p-[5px] !px-[10px]"
-        >
-          +
+        <button onClick={() => { setIsCreateModelShow(true) }} className='btnBlue rounded-[5px] mb-4 text-[20px] !p-[5px] !px-[10px]'>+
         </button>
       </div>
 
@@ -44,6 +41,29 @@ const Home = () => {
        </div>
         }
       </div>
+
+     { isCreateModelShow &&
+       <div className="createModelCon fixed top-0 left-0 right-0 bottom-0 w-screen h-screen bg-[rgb(0,0,0,0.1)] flex items-center justify-center">
+       <div className="createModel w-[25vw] h-[27vh] shadow-lg shadow-black/50 bg-[#141414] rounded-[10px] p-[20px]">
+       <h3 className='text-2xl'>Create New Project</h3>
+       <div className="inputBox !bg-[#202020] mt-4">
+               <input
+                 // onChange={(e) => { setProjTitle(e.target.value) }}
+                 // value={projTitle}
+                 type="text"
+                 placeholder='Project Title'
+               />
+             </div>
+             <div className='flex items-center gap-[10px] w-full mt-2'>
+               <button className='btnBlue rounded-[5px] w-[49%] mb-4 !p-[5px] !px-[10px] !py-[10px]'>Create</button>
+               <button onClick={() => { setIsCreateModelShow(false) }} className='btnBlue !bg-[#1A1919] rounded-[5px] mb-4 w-[49%] !p-[5px] !px-[10px] !py-[10px]'>Cancel</button>
+             </div>
+       </div>
+       </div> 
+
+     }
+
+
     </>
   );
 };
