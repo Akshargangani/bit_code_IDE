2cab159cd676d10119c8f2a27b5697f9408eb573
your name <your-email@example.com>
2025-03-03 21:33:45 +0530

    Add GridCard component with delete modal

--- project/src/components/GridCard.jsx
+++ project/src/components/GridCard.jsx
@@ -1,8 +1,10 @@
-import React from 'react'
+import React, { useState } from 'react'
 import deleteImg from "../images/delete.png"
 import codeImg from  "../images/code.png"
 
 const GridCard = () => {
+    const [isDeleteModelShow, setIsDeleteModelShow] = useState(false);
+  
   return (
    <>
      <div className="gridCard bg-[#141414] w-[270px] p-[10px] h-[180px] cursor-pointer hover:bg-[#202020] rounded-lg shadow-lg shadow-black/50">
@@ -11,9 +13,22 @@ const GridCard = () => {
       
       <div className='flex items-center justify-between'>
           <p className='text-[14px] text-[gray]'>Created in 3 mon 2025</p>
-          <img  className='w-[30px] cursor-pointer' src={deleteImg} alt="" />
+          <img  onClick={()=>{setIsDeleteModelShow(true)}} className='w-[30px] cursor-pointer' src={deleteImg} alt="" />
           </div>
           </div>
+
+          {
+       isDeleteModelShow ? <div className="model fixed top-0 left-0 w-screen h-screen bg-[rgba(0,0,0,0.1)] flex justify-center items-center flex-col">
+      <div className="mainModel w-[22vw] h-[15vh] bg-[#141414] rounded-lg p-[20px]">
+      <h3 className='text-2xl'>Do you want to delete <br />
+      this project</h3> 
+      <div className='flex w-full mt-5 items-center gap-[10px]'>
+      <button onClick={()=>{deleteProj(item._id)}} className='p-[10px] rounded-lg bg-[#FF4343] text-white cursor-pointer min-w-[49%] min-h-[30%]'>Delete</button>
+      <button onClick={()=>{setIsDeleteModelShow(false)}} className='p-[10px] rounded-lg bg-[#1A1919] text-white cursor-pointer min-w-[49%] min-h-[30%]'>Cancel</button>
+      </div>
+      </div>
+         </div> : ""
+     }  
    </>
   )
 }
